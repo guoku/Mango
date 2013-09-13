2fd862099a6a7a9a8d5075b8b022ea3cfddab3cb
zlu <zlu@workstation.(none)>
2013-09-13 20:44:00 +0800

    add setup script

---
+++ setup_script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install vim
+
+
+sudo apt-get install gcc g++ bzip2 openssl
+sudo apt-get install libssl-dev
+sudo apt-get install libzlib-ruby
+sudo apt-get install build-essential libopenssl-ruby libfcgi-dev
+sudo apt-get install autoconf automake libtool
+
+sudo apt-get install mysql-server redis-server mongodb
+sudo apt-get install python-dev
+sudo apt-get install python-setuptools
+sudo apt-get install libxml2-dev libxslt-dev
+sudo apt-get install python-lxml
+sudo apt-get install python-pip
+sudo apt-get install libmysqld-dev libmysqlclient-dev 
+sudo apt-get install python-pycurl
+sudo apt-get install libmagickwand-dev
+
+sudo pip install BeautifulSoup4
+sudo pip install pymongo 
+sudo pip install mysql-python
+
+sudo pip install django==1.5.3
+sudo pip install django-celery
+sudo pip install django-haystack
+sudo pip install mongoengine
+sudo pip install django-debug-toolbar
+sudo pip install pysolr
+sudo pip install django-mailgun
+sudo pip install django-redis-sessions
+sudo pip install django-redis-cache
+sudo pip install redis
+sudo pip install redis_session_backend 
+sudo pip install gunicorn
+sudo pip install django-storages
+sudo pip install pyapns
+sudo pip install Wand
+sudo pip install django-sphinx
+sudo pip install Hiredis
