e20c46b3d1080b315aa458540b84f0a033d91487
shahwuang <shahuwang@qq.com>
2014-01-03 19:59:45 +0800

    update fetchfailed.go

--- management/jobs/fetchfailed.go
+++ management/jobs/fetchfailed.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"Mango/management/crawler"
 	"Mango/management/utils"
 	"github.com/qiniu/log"
 	"labix.org/v2/mgo/bson"
-	"time"
 )
 
 const (
@@ -28,61 +28,81 @@ func run() {
 	mgoMango := utils.MongoInit(MGOHOST, MANGO, "taobao_items_depot")
 	log.Info("start to refetch")
 	iter := mgofailed.Find(nil).Iter()
-	failed := new(utils.FailedPages)
+	failed := new(crawler.FailedPages)
 	for iter.Next(&failed) {
 		info, err := mgofailed.RemoveAll(bson.M{"itemid": failed.ItemId})
 		if err != nil {
 			log.Info(info.Removed)
 			log.Info(err.Error())
 		}
-		page, err, detail := utils.Fetch(failed.ItemId, failed.ShopType)
+		page, detail, instock, err := crawler.FetchItem(failed.ItemId, failed.ShopType)
 		if err != nil {
-			if err.Error() == "404" {
-				log.Info("start to remove item")
-				_, err = mgofailed.RemoveAll(bson.M{"itemid": failed.ItemId})
-				if err != nil {
-					log.Info(err.Error())
-				}
+			if instock {
+				crawler.SaveFailed(failed.ItemId, failed.ShopId, failed.ShopType, mgofailed)
 			} else {
-				log.Infof("%s refetch failed\n", failed.ItemId)
-				newfail := utils.FailedPages{ItemId: failed.ItemId, ShopId: failed.ShopId, ShopType: failed.ShopType, UpdateTime: time.Now().Unix(), InStock: failed.InStock}
-				err = mgofailed.Insert(&newfail)
-				if err != nil {
-					log.Info(err.Error())
-					mgofailed.Update(bson.M{"itemid": failed.ItemId}, bson.M{"$set": newfail})
-				}
+				mgofailed.RemoveAll(bson.M{"itemid": failed.ItemId})
 			}
+			/*
+				if err.Error() == "404" {
+					log.Info("start to remove item")
+					_, err = mgofailed.RemoveAll(bson.M{"itemid": failed.ItemId})
+					if err != nil {
+						log.Info(err.Error())
+					}
+				} else {
+					log.Infof("%s refetch failed\n", failed.ItemId)
+					newfail := utils.FailedPages{ItemId: failed.ItemId, ShopId: failed.ShopId, ShopType: failed.ShopType, UpdateTime: time.Now().Unix(), InStock: failed.InStock}
+					err = mgofailed.Insert(&newfail)
+					if err != nil {
+						log.Info(err.Error())
+						mgofailed.Update(bson.M{"itemid": failed.ItemId}, bson.M{"$set": newfail})
+					}
+				}
+			*/
 		} else {
 			log.Info("%s refetch successed", failed.ItemId)
-			info, missing, err := utils.Parse(page, detail, failed.ItemId, failed.ShopId, failed.ShopType)
-			instock := true
-			parsed := false
-			if err != nil {
-				if missing {
-					parsed = true
-					instock = false
-				} else if err.Error() != "聚划算" {
-					parsed = false
-					if err.Error() == "cattag" {
-						instock = false
-					}
-
-				}
-				log.Info(err.Error())
-			} else {
-				instock = info.InStock
-				err = utils.Save(info, mgoMango)
+			info, instock, err := crawler.ParsePage(page, detail, failed.ItemId, failed.ShopId, failed.ShopType)
+			/*
+				instock := true
+				parsed := false
 				if err != nil {
-					log.Info(err.Error())
+					if missing {
+						parsed = true
+						instock = false
+					} else if err.Error() != "聚划算" {
+						parsed = false
+						if err.Error() == "cattag" {
+							instock = false
+						}
 
+					}
+					log.Info(err.Error())
+				} else {
+					instock = info.InStock
+					err = utils.Save(info, mgoMango)
+					if err != nil {
+						log.Info(err.Error())
+
+					}
 				}
-			}
-			successpage := utils.Pages{ItemId: failed.ItemId, ShopId: failed.ShopId, ShopType: failed.ShopType, FontPage: page, DetailPage: detail, UpdateTime: time.Now().Unix(), Parsed: parsed, InStock: instock}
-			err = mgopages.Insert(&successpage)
+			*/
+
 			if err != nil {
-				log.Info(err.Error())
-				mgopages.Update(bson.M{"itemid": failed.ItemId}, bson.M{"$set": successpage})
+				continue
 			}
+			instock = info.InStock
+			err = crawler.Save(info, mgoMango)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			crawler.SaveSuccessed(failed.ItemId, failed.ShopId, failed.ShopType, page, detail, true, instock, mgopages)
+			//	successpage := crawler.Pages{ItemId: failed.ItemId, ShopId: failed.ShopId, ShopType: failed.ShopType, FontPage: page, DetailPage: detail, UpdateTime: time.Now().Unix(), Parsed: true, InStock: instock}
+			//	err = mgopages.Insert(&successpage)
+			//	if err != nil {
+			//		log.Info(err.Error())
+			//		mgopages.Update(bson.M{"itemid": failed.ItemId}, bson.M{"$set": successpage})
+			//	}
 		}
 	}
 	if err := iter.Close(); err != nil {
