2624a01a48f2bdf963a24a3e848227c8ae8e60d7
shahwuang <shahuwang@qq.com>
2014-04-09 20:21:33 +0800

    update

--- gojobs/jobs/fetchnew.go
+++ gojobs/jobs/fetchnew.go
@@ -4,73 +4,11 @@ import (
     "Mango/gojobs/crawler"
     "Mango/gojobs/log"
     "fmt"
-    "github.com/astaxie/beego"
-    "github.com/xuyu/goredis"
     "labix.org/v2/mgo"
     "labix.org/v2/mgo/bson"
     "strconv"
     "time"
 )
-
-var (
-    MGOHOST   string
-    THREADNUM int
-
-    MANGO       string
-    SHOPS_DEPOT string
-    ITEMS_DEPOT string
-
-    ZERG     string
-    PAGES    string
-    FAILED   string
-    MINERALS string
-
-    TAOBAO string = "taobao.com"
-    TMALL  string = "tmall.com"
-
-    REDIS_CLIENT *goredis.Redis
-)
-
-func init() {
-    MGOHOST = beego.AppConfig.String("mongo::server")
-    MANGO = beego.AppConfig.String("mango::mango")
-    SHOPS_DEPOT = beego.AppConfig.String("mango::shops_depot")
-    ITEMS_DEPOT = beego.AppConfig.String("mango::items_depot")
-
-    ZERG = beego.AppConfig.String("zerg::zerg")
-    PAGES = beego.AppConfig.String("zerg::pages")
-    FAILED = beego.AppConfig.String("zerg::failed")
-    MINERALS = beego.AppConfig.String("zerg::minerals")
-    var err error
-    THREADNUM, err = beego.AppConfig.Int("fetchnew::threadnum")
-    if err != nil {
-        log.Error(err)
-        THREADNUM = 1
-    }
-
-    redis_server := beego.AppConfig.String("redis::server")
-    redis_port := beego.AppConfig.String("redis::port")
-    REDIS_CLIENT, err = goredis.Dial(&goredis.DialConfig{Address: fmt.Sprintf("%s:%s", redis_server, redis_port)})
-    if err != nil {
-        panic(err)
-    }
-
-}
-
-func sadd(key, value string) {
-    _, err := REDIS_CLIENT.SAdd(key, value)
-    if err != nil {
-        log.ErrorfType("redis err", "%s", err.Error())
-    }
-    num, err := REDIS_CLIENT.SCard(key)
-    if err != nil {
-        log.ErrorfType("redis err", "%s", err.Error())
-        return
-    }
-    if num == 1 {
-        REDIS_CLIENT.PExpire(key, 24*3600*1000) //设置过期时间为一天
-    }
-}
 
 type FetchNew struct {
     start bool
